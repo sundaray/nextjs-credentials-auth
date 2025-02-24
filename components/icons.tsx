b61578dd8e3a98cd4938a65b920ec83251044b8a
sundaray <pgcim14.hemant@spjimr.org>
2025-02-24 20:20:35 +0530

    added maoin navigation link

--- components/icons.tsx
+++ components/icons.tsx
@@ -1,7 +1,15 @@
-import { ArrowLeft, Eye, EyeOff, Loader, LogOut } from "lucide-react";
+import {
+  ArrowLeft,
+  ChevronDown,
+  Eye,
+  EyeOff,
+  Loader,
+  LogOut,
+} from "lucide-react";
 
 export const Icons = {
   arrowLeft: ArrowLeft,
+  chevronDown: ChevronDown,
   eye: Eye,
   eyeOff: EyeOff,
   logOut: LogOut,
---
+++ config/navbar.ts
@@ -0,0 +1,8 @@
+export const navbarLinks = {
+  main: [
+    { title: "Home", href: "/" },
+    { title: "Client", href: "/client" },
+    { title: "Server", href: "/server" },
+    { title: "Admin", href: "/admin" },
+  ],
+};
