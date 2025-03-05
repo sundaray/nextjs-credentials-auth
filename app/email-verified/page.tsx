76d565ef825186462e54fa64163ae8deccdaa6b8
sundaray <pgcim14.hemant@spjimr.org>
2025-03-05 12:31:02 +0530

    email verified page added

--- app/email-verified/page.tsx
+++ app/email-verified/page.tsx
@@ -2,20 +2,21 @@ import Link from "next/link";
 
 import { Icons } from "@/components/icons";
 
-export default function VerifyEmail() {
+export default function EmailVerified() {
   return (
-    <div className="mx-auto px-4 text-center">
+    <div className="mx-auto max-w-md px-4 text-center">
       <h2 className="mb-2 text-2xl font-semibold tracking-tight text-secondary-foreground">
-        Verify your email
+        Email verified
       </h2>
       <p className="mb-4 text-pretty text-sm text-muted-foreground">
-        We&apos;ve sent you a verification link.
+        Your email has been successfully verified. You can now sign in to access
+        your account.
       </p>
       <Link
         href="/signin"
         className="inline-flex items-center gap-1 p-2 text-sm font-semibold text-primary transition-colors hover:text-blue-500"
       >
-        Back to sign in
+        Sign in
         <Icons.arrowRight className="size-4" />
       </Link>
     </div>
